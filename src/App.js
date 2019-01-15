d8cee152e38385d84b4886f2c413b56402da7697
Tom Wayson <tom@tomwayson.com>
2019-01-15 15:42:56 -0800

    install and use reactstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -9,7 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and <Button color="primary">save</Button> to reload.
           </p>
           <a
             className="App-link"
