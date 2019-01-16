5476412bb477f38d255c8c831ee51fa39bda46e9
Tom Wayson <tom@tomwayson.com>
2019-01-16 13:26:22 -0800

    set up navbar w/ home and items routes

---
+++ src/routes/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/routes/Items.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Items() {
+  return (
+    <div>
+      <h2>Items</h2>
+    </div>
+  );
+}
+
+export default Items;
