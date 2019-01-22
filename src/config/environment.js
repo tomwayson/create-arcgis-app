74f2a32e52f8cad1b54b353d44f8d3f11808c2bd
Tom Wayson <tom@tomwayson.com>
2019-01-22 13:50:15 -0800

    read enviorment settings from a config file

---
+++ src/config/environment.js
@@ -0,0 +1,8 @@
+export default {
+  // OAuth settings
+  // see: https://esri.github.io/arcgis-rest-js/guides/browser-authentication/
+  clientId: 'EICkmTOXkBhPwIRp',
+  portal: 'https://www.arcgis.com/sharing/rest',
+  // app cookies will be prefixed with this, ex: caa_session
+  cookiePrefix: 'caa'
+}
