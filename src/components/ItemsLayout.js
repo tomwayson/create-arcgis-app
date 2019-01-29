fa2c8729c42ead04c09d1751f059c9c3fab9c8a4
Tom Wayson <tom@tomwayson.com>
2019-01-28 21:16:50 -0800

    convert ItemsLayout to a stateless functional component

--- src/components/ItemsLayout.js
+++ src/components/ItemsLayout.js
@@ -4,58 +4,53 @@ import AgoSearch from './AgoSearch';
 import ExtentsMap from './ExtentsMap';
 import ItemsTable from './ItemsTable';
 
-class ItemsLayout extends React.Component {
-  onSearch = q => {
-    const { onParamsChange } = this.props;
+function ItemsLayout({ results, total, num, q, start, onParamsChange }) {
+  function onSearch(q) {
     if (onParamsChange) {
       onParamsChange(q);
     }
-  };
-  changePage = page => {
+  }
+  function changePage(page) {
     // calculate next start record based on the number of records per page
-    const { num, onParamsChange, q } = this.props;
     const start = (page - 1) * num + 1;
     if (onParamsChange) {
       onParamsChange(q, start);
     }
-  };
-  render() {
-    const { results, total, num, q, start } = this.props;
-    // compute current page number based on start record
-    // and the number of records per page
-    const pageNumber = (start - 1) / num + 1;
-    return (
-      <>
-        <div className="row mb-2">
-          <div className="col-9">
-            <h2>
-              Your search for "{q}" yielded {total} items
-            </h2>
-          </div>
-          <div className="col-3">
-            <AgoSearch
-              q={q}
-              onSearch={this.onSearch}
-              className="search-form-inline"
-              size="sm"
-            />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <ExtentsMap items={results} />
-            <ItemsTable items={results} />
-            <ItemPager
-              pageSize={num}
-              totalCount={total}
-              pageNumber={pageNumber}
-              changePage={this.changePage}
-            />
-          </div>
-        </div>
-      </>
-    );
   }
+  // compute current page number based on start record
+  // and the number of records per page
+  const pageNumber = (start - 1) / num + 1;
+  return (
+    <>
+      <div className="row mb-2">
+        <div className="col-9">
+          <h2>
+            Your search for "{q}" yielded {total} items
+          </h2>
+        </div>
+        <div className="col-3">
+          <AgoSearch
+            q={q}
+            onSearch={onSearch}
+            className="search-form-inline"
+            size="sm"
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <ExtentsMap items={results} />
+          <ItemsTable items={results} />
+          <ItemPager
+            pageSize={num}
+            totalCount={total}
+            pageNumber={pageNumber}
+            changePage={changePage}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ItemsLayout;
